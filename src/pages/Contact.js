f7d64f0436a61378587ad76b83e65e183ae1fa3c
Colm O'Sullivan <colm.osullivan1@gmail.com>
2023-02-22 14:32:09 -0500

    Just a lil commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,13 +7,16 @@ import Typewriter from '../components/Typewriter';
 const Contact = () => {
     return(
         <AnimatedPage>
-            <section className='card'>
-                <Typewriter/>
+            <section className='contact-section flex-container card'>
+                
                 <h2>Please reach out</h2>
+                {/* <Typewriter/> */}
                 <p>You can reach me via LinkedIn, E-mail or messenger pigeon.</p>
                 <p>Here's how you can do those things!</p>
-                <AiOutlineMail/>
-                <FaLinkedin/>
+                <div className="icon-container flex-container">
+                    <a className='contact-icon' href="https://www.linkedin.com/in/colm-o-sullivan-9163baa6/"><FaLinkedin/></a>
+                    <a className='contact-icon' href="malito:colm.osullivan1@gmail.com"><AiOutlineMail/></a>
+                </div>
             </section>
         </AnimatedPage>
     )
